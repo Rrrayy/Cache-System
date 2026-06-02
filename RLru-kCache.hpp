9bcd87c440ec956202bba6fe97b9c94914794d70
Rrrayy <3111937934@qq.com>
2026-06-02 22:50:52 +0800

    Implement correct RLruKCache logic and behavior

--- RLru-kCache.hpp
+++ RLru-kCache.hpp
@@ -1,64 +1,85 @@
 #pragma once
-#include"RLruCache.hpp"
-#include<memory>
+#include "RLruCache.hpp"
+#include <memory>
+#include <unordered_map>
+
 namespace RrCache{
 
-//LRU-k LRU优化版:数据被访问k次才可以进入缓存
-    template<typename Key,typename Value>
-    class RLruKCache:public RLruCache<Key,Value>
-    {
-        private:
-            int k_;//进入缓存的评判标准，如果数据被访问k次，则可进入缓存
-            std::unique_ptr<RLruKCache<Key,size_t>>historyList_;//访问数据历史记录
-            std::unordered_map<Key,Value>historyValueMap_;//存储未达到k次访问的数据值
-            
-        public:
-            RLruKCache(int capacity,int historyCapacity,int k)
-                :RLruCache<Key,Value>(capacity)
-                ,historyList_(std::make_unique<RLruKCache<Key,size_t>>(historyCapacity))
-                ,k_(k){}
-            Value get(Key key){
-                Value value{};
-                bool inMainCache=RLruCache<Key,Value>::get(key,value);
-                size_t historyCount=historyList_->get(key);
-                historyCount++;
-                historyList_->put(key,historyCount);
-                if(inMainCache){
-                    return value;
-                }
-                if(historyCount>=k_){
-                    auto it =historyValueMap_.find(key);
-                    if(it!=historyValueMap_.end()){
-                        Value storedValue=it->second;
-                        historyList_->remove(key);
-                        historyValueMap_.erase(it);
-                        RLruCache<Key,Value>::put(key,storedValue);
-                        return storedValue;
-                    }
-                }
-                return value;
-            }
-            void put(Key key,Value value){
-                Value existingValue{};
-                bool inMainCache=RLruCache<Key,Value>::get(key,existingValue);
-                if(inMainCache){
-                    //已经在主缓存，直接更新
-                    RLruCache<Key,Value>::put(key,value);
-                    return;
-                }
-                //获取并更新访问历史
-                size_t historyCount=historyList_->get(key);
-                historyCount++;
-                historyList_->put(key,historyCount);
-                //保存值到历史记录映射，供后续get操作使用
-                historyValueMap_[key]=value;
-                //检查访问次数是否到达K
-                if(historyCount>=k_){
-                    historyList_->remove(key);
-                    historyValueMap_.erase(key);
-                    RLruCache<Key,Value>::put(key,value);
-                }
+template<typename Key, typename Value>
+class RLruKCache : public RLruCache<Key, Value>
+{
+private:
+    int k_;
+    RLruCache<Key, size_t>* historyList_;
+    std::unordered_map<Key, Value> historyValueMap_;
 
+public:
+    RLruKCache(int capacity, int historyCapacity, int k)
+        : RLruCache<Key, Value>(capacity), k_(k) {
+        historyList_ = new RLruCache<Key, size_t>(historyCapacity);
+    }
+
+    ~RLruKCache() override {
+        delete historyList_;
+    }
+
+    Value get(Key key) override {
+        Value value{};
+        bool inMainCache = RLruCache<Key, Value>::get(key, value);
+        size_t historyCount = historyList_->get(key);
+        historyCount++;
+        historyList_->put(key, historyCount);
+        if (inMainCache) {
+            return value;
+        }
+        if (historyCount >= k_) {
+            auto it = historyValueMap_.find(key);
+            if (it != historyValueMap_.end()) {
+                Value storedValue = it->second;
+                historyList_->remove(key);
+                historyValueMap_.erase(it);
+                RLruCache<Key, Value>::put(key, storedValue);
+                return storedValue;
             }
-    };
-}
+        }
+        return value;
+    }
+
+    void put(Key key, Value value) override {
+        Value existingValue{};
+        bool inMainCache = RLruCache<Key, Value>::get(key, existingValue);
+        if (inMainCache) {
+            RLruCache<Key, Value>::put(key, value);
+            return;
+        }
+        auto it = historyValueMap_.find(key);
+        if (it != historyValueMap_.end()) {
+            it->second = value;
+        } else {
+            historyValueMap_[key] = value;
+        }
+        size_t historyCount = historyList_->get(key);
+        historyCount++;
+        historyList_->put(key, historyCount);
+        if (historyCount >= k_) {
+            historyList_->remove(key);
+            auto mapIt = historyValueMap_.find(key);
+            if (mapIt != historyValueMap_.end()) {
+                Value storedValue = mapIt->second;
+                historyValueMap_.erase(mapIt);
+                RLruCache<Key, Value>::put(key, storedValue);
+            }
+        }
+    }
+
+    bool get(Key key, Value& value) override {
+        Value v = this->get(key);
+        if (v != Value{}) {
+            value = v;
+            return true;
+        }
+        return false;
+    }
+};
+
+} // namespace RrCache
