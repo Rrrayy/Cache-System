6b29aa10ce1e5b098994eeb75de3bd228ce7e2eb
Rrrayy <3111937934@qq.com>
2026-09-04 21:48:38 +0800

    优化缓存抽象接口的参数传递与析构语义

--- cache_system.hpp
+++ cache_system.hpp
@@ -3,13 +3,13 @@ namespace RrCache{
     template<typename Key,typename Value>
     class cache_system{
         public:
-            virtual ~cache_system() {};
+            virtual ~cache_system()=default;
             //添加缓存接口
-            virtual void put(Key key,Value value)=0;
+            virtual void put(const Key& key,const Value& value)=0;
             //传入key，判断是否存在对应value，若存在则用参数得到value值
-            virtual bool get(Key key,Value &value)=0;
+            virtual bool get(const Key& key,Value &value)=0;
             //若找到key，则直接返回value
-            virtual Value get(Key key)=0;
+            virtual Value get(const Key& key)=0;
 
     };
 }
